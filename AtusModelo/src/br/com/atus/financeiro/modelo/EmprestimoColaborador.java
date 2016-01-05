002117302032f8f2232fd67d4f36edbdb7bae52d
José Ari Vasconcelos Aguiar Filho <aricomputacao@gmail.com>
2016-01-05 14:18:40 -0300

    cadastero aluno

---
+++ AtusModelo/src/br/com/atus/financeiro/modelo/EmprestimoColaborador.java
@@ -0,0 +1,147 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.atus.financeiro.modelo;
+
+import br.com.atus.cadastro.modelo.Colaborador;
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.Objects;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotNull;
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+
+/**
+ *
+ * @author ari
+ */
+@Entity
+@Table(name = "emprestimo_colaborador",schema = "financeiro")
+@Cache(usage = CacheConcurrencyStrategy.NONSTRICT_READ_WRITE)
+public class EmprestimoColaborador implements Serializable{
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "emc_id",nullable = false)
+    private Long id;
+    
+    @Temporal(TemporalType.DATE)
+    @Column(name = "emc_data",nullable = false)
+    private Date dataEmprestimo;
+    
+    @Temporal(TemporalType.DATE)
+    @Column(name = "emc_data_pagamento")
+    private Date dataPagamento;
+    
+    @NotNull
+    @ManyToOne
+    @JoinColumn(name = "col_id",referencedColumnName = "col_id",nullable = false)
+    private Colaborador colaborador;
+    
+    @NotNull
+    @Column(name = "emc_valor",nullable = false)
+    private BigDecimal valor;
+    
+    @NotNull
+    @Column(name = "emc_ativo",nullable = false)
+    private boolean  ativo;
+    
+    @Column(name = "emc_observacao",length = 1024)
+    private String observacao;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Date getDataEmprestimo() {
+        return dataEmprestimo;
+    }
+
+    public void setDataEmprestimo(Date dataEmprestimo) {
+        this.dataEmprestimo = dataEmprestimo;
+    }
+
+    public Colaborador getColaborador() {
+        return colaborador;
+    }
+
+    public void setColaborador(Colaborador colaborador) {
+        this.colaborador = colaborador;
+    }
+
+    public BigDecimal getValor() {
+        return valor;
+    }
+
+    public void setValor(BigDecimal valor) {
+        this.valor = valor;
+    }
+
+    public boolean isAtivo() {
+        return ativo;
+    }
+
+    public void setAtivo(boolean ativo) {
+        this.ativo = ativo;
+    }
+
+    public String getObservacao() {
+        return observacao;
+    }
+
+    public void setObservacao(String observacao) {
+        this.observacao = observacao;
+    }
+
+    public Date getDataPagamento() {
+        return dataPagamento;
+    }
+
+    public void setDataPagamento(Date dataPagamento) {
+        this.dataPagamento = dataPagamento;
+    }
+
+    
+    
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 67 * hash + Objects.hashCode(this.id);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final EmprestimoColaborador other = (EmprestimoColaborador) obj;
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        return true;
+    }
+    
+    
+    
+}
