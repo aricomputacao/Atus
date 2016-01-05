9b4fdf0c91c7b11b53fb7dde61ab93b420aa9d4c
José Ari Vasconcelos Aguiar Filho <aricomputacao@gmail.com>
2016-01-05 15:06:29 -0300

    cadastero aluno

--- AtusModelo/src/br/com/atus/util/NumeroPorExtensoTests.java
+++ AtusModelo/src/br/com/atus/util/NumeroPorExtensoTests.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.*;
 
 import java.math.BigDecimal;
 import java.math.BigInteger;
+import static org.hibernate.validator.internal.util.Contracts.assertNotNull;
 
 import org.junit.Test;
 
